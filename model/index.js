2fb0492742ede4a5d558f7d2b9539b46cd57a9d6
OlgaTomilko <67008325+OlgaTomilko@users.noreply.github.com>
2021-05-08 23:46:48 +0300

    Сделала раутинг для работы с базой контактов

--- model/index.js
+++ model/index.js
@@ -1,15 +1,40 @@
 // const fs = require('fs/promises')
 // const contacts = require('./contacts.json')
+const { v4: uuid } = require("uuid");
+const db = require("./db");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  return db.get("contacts").value();
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const id = contactId.toString();
+  return db.get("contacts").find({ id }).value();
+};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  const id = contactId.toString();
+  const [record] = db.get("contacts").remove({ id }).write();
+  return record;
+};
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  const id = uuid();
+  const record = {
+    id,
+    ...body,
+  };
+  db.get("contacts").push(record).write();
+  return record;
+};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  const id = contactId.toString();
+  const record = db.get("contacts").find({ id }).assign(body).value();
+
+  db.write();
+  return record.id ? record : null;
+};
 
 module.exports = {
   listContacts,
@@ -17,4 +42,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,75 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const Contacts = require("../../model/index");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const contacts = await Contacts.listContacts();
+    return res.json({ status: "success", code: 200, data: { contacts } });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const contact = await Contacts.getContactById(req.params.contactId);
+    if (contact) {
+      return res
+        .status(200)
+        .json({ status: "success", code: 200, data: { contact } });
+    }
+    return res
+      .status(404)
+      .json({ status: "error", code: 404, message: "Not Found" });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  try {
+    const contact = await Contacts.addContact(req.body);
+    return res
+      .status(201)
+      .json({ status: "success", code: 201, data: { contact } });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const contact = await Contacts.removeContact(req.params.contactId);
+    if (contact) {
+      res.status(200).json({ status: "success", code: 200, data: { contact } });
+    }
+    return res
+      .status(404)
+      .json({ status: "error", code: 404, message: "Not Found" });
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    const contact = await Contacts.updateContact(
+      req.params.contactId,
+      req.body
+    );
+    if (contact) {
+      return res
+        .status(200)
+        .json({ status: "success", code: 200, data: { contact } });
+    }
+    return res
+      .status(404)
+      .json({ status: "error", code: 404, message: "Not Found" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
